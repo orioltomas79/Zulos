a85ca0f68cf3b87000333d0d188cbae6e5e946f0
Tomas Castelltort <OLTS@gft.com>
2017-07-28 17:05:24 +0200

    First Commit

---
+++ DataSources/Ideal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+
+namespace Zulos.DataSources
+{
+    public class Ideal
+    {
+
+        public static List<Zulos.Model.Zulo> GetZulos()
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+                string page = "https://www.idealista.com/venta-viviendas/cerdanya-francesa/con-precio-hasta_180000,precio-desde_100000,metros-cuadrados-mas-de_40,de-dos-dormitorios,de-tres-dormitorios,de-cuatro-cinco-habitaciones-o-mas/";
+
+                HttpResponseMessage response = client.GetAsync(page).Result;
+                string stringData = response.Content.ReadAsStringAsync().Result;
+                response.Dispose();
+               
+                var allIndexOf = AllIndexOf(stringData, "<article>", StringComparison.OrdinalIgnoreCase);
+                Console.WriteLine(string.Join(",", allIndexOf));  // 9,27
+
+            }
+
+            return null;
+
+        }
+
+
+        public static System.Collections.Generic.IList<int> AllIndexOf(string text, string str, StringComparison comparisonType)
+        {
+            List<int> allIndexOf = new List<int>();
+            int index = text.IndexOf(str, comparisonType);
+            while (index != -1)
+            {
+                allIndexOf.Add(index);
+                index = text.IndexOf(str, index + str.Length, comparisonType);
+            }
+            return allIndexOf;
+        }
+    }
+}
---
+++ Model/Zulo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Zulos.Model
+{
+    public class Zulo
+    {
+        public string Source {get; set;}
+        public string Id {get; set;}
+        public string Name {get; set;}
+        public string Url {get; set;}
+        public int Price {get; set;}
+    }
+}
